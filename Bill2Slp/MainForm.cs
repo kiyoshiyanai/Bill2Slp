3b547754d422987fa07b5b0c3d359f4beb53f083
kiyoshi.yanai <kyanai@s01.itscom.net>
2017-06-07 16:51:24 +0900

    ボタンは実装

---
+++ Bill2Slp/MainForm.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bill2Slp
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private DateTime GetPrefMonth(DateTime date)
+        {
+            return date.AddDays(-date.Day);
+        }
+
+        private string GetFileName()
+        {
+            string s = "";
+            if (true)
+            {
+
+            }
+            return s;
+        }
+    }
+       
+}
